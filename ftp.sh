17fa2e14b5dfe4cc2483e06c5b4f57dd3ee904ed
Samuel Kadima <86892156+KadiSam01@users.noreply.github.com>
2025-03-01 13:47:22 -0500

    ftp1.sh

--- ftp.sh
+++ ftp.sh
@@ -1,41 +1,43 @@
 #!/bin/bash
 
-# Define the allowed FTP users
-FTP_USERS=(
-  camille_jenatzy gaston_chasseloup leon_serpollet william_vanderbilt henri_fournier
-  maurice_augieres arthur_duray henry_ford louis_rigolly pierre_caters paul_baras
-  victor_hemery fred_marriott lydston_hornsted kenelm_guinness rene_thomas
-  ernest_eldridge malcolm_campbell ray_keech john_cobb dorothy_levitt paula_murphy
-  betty_skelton rachel_kushner kitty_oneil jessi_combs andy_green
+# Define the target directory
+FTP_DIRECTORY="/mnt/files"
+
+# Ensure the directory exists
+mkdir -p "$FTP_DIRECTORY"
+
+# List of files to create
+FILES=(
+    "iron_cross.data"
+    "3_point_molly.data"
+    "dark_side.data"
+    "come_dont_come.data"
+    "odds.data"
+    ".house_secrets.data"
+    "pass_line.data"
+    "risky_roller.data"
+    "covered_call.data"
+    "married_put.data"
+    "bull_call.data"
+    "protective_collar.data"
+    "long_straddle.data"
+    "long_strangle.data"
+    "long_call_butteryfly.data"
+    "iron_condor.data"
+    "iron_butterfly.data"
+    "short_put.data"
+    "data_dump_1.bin"
+    "data_dump_2.bin"
+    "data_dump_3.bin"
+    "datadump.bin"
 )
 
-# Path to vsftpd user list
-VSFTPD_USER_LIST="/etc/vsftpd.user_list"
-
-# Backup the existing user list
-cp $VSFTPD_USER_LIST ${VSFTPD_USER_LIST}.bak
-
-# Add allowed users to vsftpd.user_list
-echo "Updating FTP allowed users..."
-> $VSFTPD_USER_LIST  # Clear existing file
-for user in "${FTP_USERS[@]}"; do
-  echo "$user" >> $VSFTPD_USER_LIST
+# Create each file
+for file in "${FILES[@]}"; do
+    touch "$FTP_DIRECTORY/$file"
 done
 
-# Ensure vsftpd.conf allows user_list
-VSFTPD_CONF="/etc/vsftpd.conf"
-if ! grep -q "^userlist_enable=YES" $VSFTPD_CONF; then
-  echo "userlist_enable=YES" >> $VSFTPD_CONF
-fi
-if ! grep -q "^userlist_file=$VSFTPD_USER_LIST" $VSFTPD_CONF; then
-  echo "userlist_file=$VSFTPD_USER_LIST" >> $VSFTPD_CONF
-fi
-if ! grep -q "^userlist_deny=NO" $VSFTPD_CONF; then
-  echo "userlist_deny=NO" >> $VSFTPD_CONF
-fi
+# Set correct permissions (optional)
+chmod 644 "$FTP_DIRECTORY"/*
 
-# Restart vsftpd service
-echo "Restarting vsftpd..."
-systemctl restart vsftpd
-
-echo "FTP users successfully updated and vsftpd restarted!"
+echo "All files have been created in $FTP_DIRECTORY"
